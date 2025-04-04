a80945ff56a5a8c4c246d95554aabb7979c2b13a
James-Frown <21122@ctca.co.za>
2025-04-04 12:10:31 +0200

    feat(api): fiexed and updated the api paths and data

--- crm/urls.py
+++ crm/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import ArtistListCreateView, SongListCreateView, AppearanceListCreateView
+from .views import (
+    ArtistListCreateView, ArtistDetailView, ActiveArtistListView, SongListCreateView, SongDetailView, AppearanceListCreateView, AppearanceDetailView
+)
 
 urlpatterns = [
-    path('artists/', ArtistListCreateView.as_view(), name='artist-list-create'),
-    path('songs/', SongListCreateView.as_view(), name='song-list-create'),
-    path('appearances/', AppearanceListCreateView.as_view(), name='appearance-list-create'),
+    # Artist URLs
+    path('api/artists/', ArtistListCreateView.as_view(), name='artist-list-create'),
+    path('api/artists/<int:pk>/', ArtistDetailView.as_view(), name='artist-detail'),
+    path('api/artists/active/', ActiveArtistListView.as_view(), name='active-artist-list'),
+    # Song URLs
+    path('api/songs/', SongListCreateView.as_view(), name='song-list-create'),
+    path('api/songs/<int:pk>/', SongDetailView.as_view(), name='song-detail'),
+    # Appearance URLs
+    path('api/appearances/', AppearanceListCreateView.as_view(), name='appearance-list-create'),
+    path('api/appearances/<int:pk>/', AppearanceDetailView.as_view(), name='appearance-detail'),
 ]
